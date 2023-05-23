cf85f581222151f37ca2dbf498c1b78ec6f29e2b
vortex <you@example.com>
2023-05-24 01:45:40 +0300

    https instead of http

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Menu from './Menu';
 import Info from './Info';
 
 function App() {
-    const baseURL = 'http://api.weatherapi.com/v1/';
+    const baseURL = 'https://api.weatherapi.com/v1/';
 
     const currentPart = 'current.json?key=ddb3c04efbbc46ad9d6201356230905&q=';
     const forecastPart = 'forecast.json?key=ddb3c04efbbc46ad9d6201356230905&q=';
